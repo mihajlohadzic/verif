36c0b0d536e4c7f3e9bc5bab1324c94a8e2a6348
Mihajlo Hadzic <mihajlohadzic99@gmail.com>
2023-04-20 14:25:13 +0200

    new driver

---
+++ driver_laplas/app/app.cpp
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include<cstdlib>
+using namespace std;
+
+#define MASK_SIZE 25
+#define PICTURE_SIZE 100
+void write_ip(const int start_stop, const int position, const int value);
+void read_ip_result(std::vector<int> &result);
+void print_result(std::vector<int> &result);
+
+int width = 10;
+int height = 10;
+int border1 = 2;
+int l1 = 1;
+int START = 1;
+int STOP = 0;
+int test_for_read = 0;
+int start_stop;
+
+
+//int mask[MASK_SIZE] = {0,0,-1,0,0,0,-1,-2,-1,0,-1,-2,16,-2,-1,0,-1,-2,-1,0,0,0,-1,0,0};
+
+int main ()
+{   
+    srand (time(NULL));
+    int random;
+
+    std::vector<int> result;
+    for(int i = 0; i < width*height; i++)
+    {
+        result.push_back(0);
+    }
+    int mask[MASK_SIZE] = {0,0,-1,0,0,0,-1,-2,-1,0,-1,-2,16,-2,-1,0,-1,-2,-1,0,0,0,-1,0,0};
+    
+    int picture[PICTURE_SIZE] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,1,2,3,4,5,6,7,8,9,10,11,12,13,14,1,2,3,4,5,6,7,8,9,10,11,12,13,14, 1,2,3,4,5,6,7,8,9,10,11,12,13,14, 1,2,3,4,5,6,7,8,9,10,11,12,13,14, 1,2,3,4,5,6,7,8,9,10,11,12,13,14, 1,2,3,4,5,6,7,8,9,10,11,12,13,14, 11,11};
+    int picture1[PICTURE_SIZE] = {13,28,39,41,52,63,71,77,88,10,81,212,153,142,199,29,32,49,55,66,77,88,129,180,191,212,213,214,241,209,153,200,155,166,187,88,9,10,11,12,13,14, 1,2,3,251,5,156,78,8,98,10,11,12,13,14, 1,2,3,4,5,6,7,8,9,10,11,12,13,14, 1,2,3,4,5,68,7,8,99,107,11,12,13,14, 1,2,3,4,5,6,78,8,9,10,191,12,13,14, 11,11};  
+	int picture2 [PICTURE_SIZE] = {38,162,95,94,161,105,209,108,22,240,131,166,84,47,51,3,198,118,82,186,64,138,29,52,177,216,2,134,222,199,181,46,184,188,212,252,206,75,254,42,32,31,208,136,176,67,24,172,26,243,20,10,16,87,25,104,133,248,4,171,81,154,169,165,40,43,210,201,228,45,78,189,128,1,99,217,187,164,185,106,71,50,143,167,112,155,92,183,247,192,58,229,220,93,146,130,142,238,205,213};
+	int picture3 [PICTURE_SIZE] = {138,134,66,14,181,238,55,156,23,179,80,143,190,104,195,248,106,158,239,131,10,122,159,213,119,109,71,9,130,243,88,150,120,38,139,94,221,24,20,42,225,126,51,198,90,202,32,167,58,56,222,36,11,234,57,50,215,194,144,33,76,229,27,220,180,84,85,105,184,28,171,152,188,205,2,87,29,59,15,118,95,135,147,185,232,226,6,148,40,142,200,241,116,63,141,199,52,101,172,3};
+    int i;
+    int j;
+    
+    
+    write_ip(STOP,0, width);
+    write_ip(STOP,1, height);
+    
+    for (i = 2; i < MASK_SIZE + 2; ++i)
+    {
+        write_ip(STOP,i, mask[i-2]);
+    }
+
+    int p = 0;
+
+    for (j = MASK_SIZE+2; j < PICTURE_SIZE+MASK_SIZE+2; j++)
+    {
+        //random = 10 + (rand() % 11);
+        random = picture[p];
+        write_ip(STOP,j,random);
+        p++;
+    }
+
+    
+    write_ip(START, 0, 0);
+    read_ip_result(result);
+    print_result(result);
+         
+
+    return 0;
+}
+
+void write_ip(const int start_stop,const int position, const int value)
+{
+    FILE *laplas;
+    laplas = fopen("/dev/xlnx,laplas", "w");
+
+        fprintf(laplas, "%d,%d,%d\n",start_stop,position,value);
+        printf(" [APP] %d ,%d, %d\n",start_stop,position,value);
+
+    fclose(laplas);
+}
+
+void read_ip_result(std::vector<int> &result)
+{
+    FILE *laplas;
+    int read_result;
+
+    laplas = fopen("/dev/xlnx,laplas", "r");
+    for (int i = 0; i < width*height; i++)
+    {
+       fscanf(laplas, "%d ", &read_result);
+       result[i]= read_result;
+    }
+
+    fclose(laplas);
+}
+void print_result(std::vector<int> &result)
+{
+    cout<<"result: ";
+    for(int i = 0; i < width*height; i++)
+    {
+        cout<<result[i]<<" ";
+        if (result[i] == 255)
+         {
+             cout << " adresa: " << i << endl <<endl;
+         } 
+    }
+    cout<<endl;
+}
---
+++ driver_laplas/driver.c
@@ -0,0 +1,409 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/platform_device.h>
+#include <asm/io.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/io.h>
+
+#include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/of_platform.h>
+
+#include <linux/version.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/delay.h>
+
+MODULE_DESCRIPTION("Driver for Laplas algorithm");
+MODULE_ALIAS("custom:laplas");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR ("laplas team");
+#define DEVICE_NAME "laplas"
+#define DRIVER_NAME "laplas"
+#define BUFF_SIZE 900
+#define MASK_SIZE 25
+#define PICTURE_SIZE 100
+//**GLOBAL VARIABLES**//
+
+int pos;
+
+int b = 0;
+int endRead = 0;
+int position;
+int value;
+int height = 0;
+int width = 0;
+int cnt_pos = 0; //mask counter
+int cnt_pos1 = 0; // picture counter
+int read_pos;
+int start_stop;
+int spix;
+int border1 = 0;
+int border2 = 0;
+
+int mask[MASK_SIZE];        //INPUT MASK FROM APP
+int mask1[5][5];            //INPUT MASK CONVERTED TO 2D ARRAY
+int picture[PICTURE_SIZE];  //INPUT PICTURE FROM APP
+int picturEv2[10][10];      //INPUT PICTURE CONVERTED TO 2D ARRAY
+int newinp[10][10];         //RESULT OF LAPLAS
+int im[10][10];             //RESULT OF ZERO CROSSING
+int result[PICTURE_SIZE];   //OUTPUT RESULT
+int im_result[PICTURE_SIZE];
+int max = 0;
+int th = 0;
+int finish = 0;
+int l1 = 5;
+int l2 = 3;
+/*
+int start_stop;
+int test_for_read = 0;
+int val=0;
+int i = 0;
+int j = 0;
+
+*/
+
+static int laplas_open(struct inode *i, struct file *pfile);
+static int laplas_close(struct inode *i, struct file *pfile);
+static ssize_t laplas_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
+static ssize_t laplas_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) ;
+static int __init laplas_init(void);
+static void __exit laplas_exit(void);
+static void laplas_hw(const int width, const int height, const int mask[], const int picture[]);
+
+
+static struct file_operations my_fops =
+{
+    
+    .owner = THIS_MODULE,
+    .open = laplas_open,
+    .read = laplas_read,
+    .write = laplas_write,
+    .release = laplas_close,
+};
+
+static dev_t my_dev_id;
+static struct class *my_class;
+static struct cdev *my_cdev;
+static struct device *my_device;
+
+static int __init laplas_init(void)
+{
+    
+    int ret = 0;
+   
+
+    ret = alloc_chrdev_region(&my_dev_id,0,1, DRIVER_NAME);
+    if(ret){
+        printk(KERN_ERR "failed to register char device\n");
+        return ret;
+    }
+    printk(KERN_INFO "char device region allocated\n");
+
+
+    my_class = class_create(THIS_MODULE, "laplas_class");
+    if(my_class == NULL)
+    {
+        printk(KERN_ERR "failed to create class\n");
+        goto fail_0;
+    }
+    printk (KERN_INFO "Class created");
+
+    //CREATE IP MODULE
+   my_device = device_create(my_class, NULL, my_dev_id, NULL, "xlnx,laplas");
+   if (my_device == NULL){
+      printk(KERN_ERR "failed to create device\n");
+      goto fail_1;
+   }
+   printk(KERN_INFO "device created\n");
+
+   my_cdev = cdev_alloc();
+   my_cdev->ops = &my_fops;
+   my_cdev->owner = THIS_MODULE;
+   ret = cdev_add(my_cdev, my_dev_id, 1);
+   if(ret)
+   {
+        printk(KERN_ERR "failed to add cdev\n");
+        goto fail_2;
+   }
+    printk(KERN_INFO "cdev added\n");
+    printk(KERN_INFO "Laplas driver\n");
+    return 0;
+    
+    fail_2:
+     device_destroy(my_class, my_dev_id);
+    fail_1:
+      class_destroy(my_class);
+    fail_0:
+      unregister_chrdev_region(my_dev_id, 1);
+
+      return -1;
+}
+
+static int laplas_open(struct inode *i, struct file *pfile)
+{
+        printk(KERN_INFO "Succesfully opened file\n");
+
+      return 0;
+} 
+static int laplas_close(struct inode *i, struct file *pfile)
+{
+    printk(KERN_INFO "Succesfully closed file\n");
+    return 0;
+}  
+
+/**READ AND WRITE**/
+ssize_t laplas_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
+{
+    printk(KERN_INFO "Welcome TO laplas_write\n");
+    char buff[BUFF_SIZE];
+    int ret;
+    
+    
+    ret = copy_from_user(buff, buffer, length);
+    if (ret)
+        return -EFAULT;
+    buff[length-1] = '\0';
+    
+    
+    ret = sscanf(buff, "%d,%d,%d",&start_stop,&position, &value);
+    if (start_stop == 0)
+    {
+            if (position == 0)
+            {
+                width = value;
+                printk(KERN_INFO "width is := %d\n", width);
+
+            }
+            else if (position == 1)
+            {
+                height = value;
+                printk(KERN_INFO "height is := %d\n", height);
+            }
+            else if (position >= 2 && position <= 26)
+            {
+                printk(KERN_INFO "cnt pos = %d\n",cnt_pos);
+                mask[cnt_pos] = value;
+                printk(KERN_INFO "mask  is := %d\n", mask[cnt_pos]);
+                cnt_pos++;   
+            }
+            else if (position >=27 && position <= 126)
+            {
+                printk(KERN_INFO "cnt pos 1 = %d\n",cnt_pos1);
+               picture[cnt_pos1] = value;
+               printk(KERN_INFO "picture is := %d\n", picture[cnt_pos1]);
+               cnt_pos1++;
+
+            }
+    }
+    else if (start_stop == 1 && position == 0 && value == 0)
+    {
+        printk(KERN_INFO "Time to start LAPLAS HW !!\n \n");
+        laplas_hw(width,height,mask,picture);
+        finish = 1;
+        printk(KERN_INFO "finish is = %d \n",finish);
+    }
+ 
+    return length;
+};
+
+
+static ssize_t laplas_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
+{
+    int ret;
+    char buff[BUFF_SIZE];
+    long int len = 0;
+    printk(KERN_INFO "Welcome to LAPLAS_READ");
+    if (endRead == 1)
+    {
+        endRead = 0;
+        read_pos = 0;
+        printk(KERN_INFO "Succesfully read from file\n");
+    }
+    
+    len = scnprintf(buff, BUFF_SIZE, "%d ",im_result[read_pos]);
+    printk(KERN_INFO "read pos is %d \n", read_pos);
+    ret = copy_to_user(buffer, buff, len);
+    if (ret)
+    {
+        return -EFAULT;    
+    }
+    read_pos++;
+    printk(KERN_INFO "read pos is %d \n", read_pos);
+    if (read_pos == width*height)
+    {
+        endRead = 1;
+    }
+    
+    
+    return len;
+}
+
+
+static void __exit laplas_exit(void)
+{
+  printk(KERN_INFO "laplas driver starting rmmod.\n");
+    cdev_del(my_cdev);
+  
+  device_destroy(my_class,my_dev_id);  
+
+  class_destroy(my_class);
+  unregister_chrdev_region(my_dev_id,1);
+  printk(KERN_INFO "laplas driver exited.\n");
+}
+
+void laplas_hw(const int width, const int height, const int mask[], const int picture[])
+{
+    
+    printk(KERN_INFO" WE ARE IN HW SIMULATION");
+    
+    
+    
+    int k = 0;
+    int m = 0;
+    int cnt = 0;
+    border1 = (l1 / 2);
+    border2 = (l2 / 2);
+    for (int i = 0; i < width; ++i)
+    {
+        for (int j = 0; j < height; ++j)
+        {
+            picturEv2[i][j] = picture[cnt];
+            newinp[i][j] = 0;
+            im[i][j] = 0;
+           
+            cnt++;
+        
+        }
+    }
+    cnt = 0;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        for (int j = 0; j < 5; ++j)
+        {
+            mask1[i][j] = mask[cnt];
+            cnt++;
+        
+        }
+    }
+
+    printk(KERN_INFO "Laplas start ! \n");
+    for (int i=border1 ;i< height-border1; i++)
+    {
+            for (int j=border1;j<(width-border1);j++)
+            {
+                spix=0;
+                for (k=0;k<l1;k++)
+                {           
+                    for (m=0;m<l1;m++)
+                    { 
+                        spix = spix+(mask1[k][m]*(picturEv2[i-border1+k][j-border1+m]));
+                        
+
+                    }
+
+                }
+                printk(KERN_INFO "spix is = %d \n", spix); 
+                newinp[i][j]=spix; // nova slika 
+            }
+    }
+    printk(KERN_INFO "Find maximum falue !\n");
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            if (newinp[i][j] > max)
+            {
+                max = newinp[i][j];
+            }
+        }
+    }
+    printk(KERN_INFO "Maximum value is = %d\n", max);
+    printk(KERN_INFO "Zero crossing and tresholding!\n");
+
+    th = (3/4)*max;
+    
+    
+    printk(KERN_INFO "tresholding is = %d\n", th);
+
+     for (int i = border2;i < (height - border2);i++)
+    {    //j=0;
+        for (int j = border2;j < (width - border2);j++)
+        {
+            if (newinp[i][j] != 0)
+            {
+                if ((newinp[i][j + 1] >= 0 && newinp[i][j - 1] < 0) || (newinp[i][j + 1] < 0 && newinp[i][j - 1] >= 0))
+                {
+                    if ((newinp[i][j] >= th))
+                    {
+                        im[i][j] = 255;
+                    }
+                }
+
+                else if ((newinp[i + 1][j] >= 0 && newinp[i - 1][j] < 0) || (newinp[i + 1][j] < 0 && newinp[i - 1][j] >= 0))
+                {
+                    if (newinp[i][j] >= th)
+                    {
+                        im[i][j] = 255;
+                    }
+                }
+
+                else if ((newinp[i + 1][j + 1] >= 0 && newinp[i - 1][j - 1] < 0) || (newinp[i + 1][j + 1] < 0 && newinp[i - 1][j - 1] >= 0))
+                {
+                    if (newinp[i][j] >= th)
+                    {
+                        im[i][j] = 255;
+                    }
+                }
+
+                else if ((newinp[i - 1][j + 1] >= 0 && newinp[i + 1][j - 1] < 0) || (newinp[i - 1][j + 1] < 0 && newinp[i + 1][j - 1] >= 0))
+                {
+                    if (newinp[i][j] >= th)
+                    {
+                        im[i][j] = 255;
+                    }
+                }
+            }
+        }
+    }
+        for (int i = 0; i < height; ++i)
+        {
+            for (int j = 0; j < width; ++j)
+            {
+                result[j+i*width] = newinp[i][j];
+                printk(KERN_INFO "result = %d , position = %d\n", result[j+i*width], j+i*width);            
+            }
+        }
+
+        for (int i = 0; i < height; ++i)
+        {
+            for (int j = 0; j < width; ++j)
+            {
+                im_result[j+i*width] = im[i][j];
+                if (im_result == 255)
+                {
+                    printk(KERN_INFO "ADRESA : position = %d\n", j+i*width);
+                }
+                            
+            }
+        }
+
+
+
+
+    printk(KERN_INFO "Laplas finish ! \n");
+
+    
+    
+}
+
+module_init(laplas_init);
+module_exit(laplas_exit);
